441054f482ee1f83eae9d8a9ef37c1f36e712252
CrazyLemur <trevornlucas@gmail.com>
2023-07-28 03:32:51 -0500

    Location sharing, logout, image upload, groupAvatr

--- utils/socketHandlers.js
+++ utils/socketHandlers.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../model/user');
 
 module.exports = function(io, state) {
   let sharedLocations = state.sharedLocations;
@@ -15,14 +16,13 @@ module.exports = function(io, state) {
     else {
       next(new Error('Authentication error: No token'));
     }
+
   }).on('connection', async (socket) => {
     console.log(socket.decoded.username, 'connected to server.');
 
-    socket.join(socket.handshake.query.groupId);
-
 // #region Message stuff
     socket.on('getMessages', (groupId) => {
-      socket.emit('UpdateMessages', messages[groupId] || []);
+      io.to(socket.currentRoom).emit('UpdateMessages', messages[groupId] || []);
     })
     
     socket.on('newMessage', (newMsg) => {
@@ -51,9 +51,41 @@ module.exports = function(io, state) {
         messages[newMsg.groupId] = [message];
       }
 
-      io.to(socket.handshake.query.groupId).emit('UpdateMessages', messages[newMsg.groupId]);
+      io.to(socket.currentRoom).emit('UpdateMessages', messages[newMsg.groupId]);
     });
 // #endregion
+    socket.on('setActiveGroup', async (groupId) => {
+      console.log(socket.decoded.username, 'setting active group to ', groupId);
+      
+      // Query database for matching user belonging to groupId
+      try {
+        const user = await User.findOne({
+          username: socket.decoded.username,
+          groups: { $in: [groupId] }
+         });
+
+        if (!user) {
+          console.error('Error: User belonging to groupId could not be found')
+          return;
+        }
+
+        // set user's currentGroup to groupId
+        user.currentGroup = groupId;
+        await user.save();
+
+        // socket leaves the current room
+        if (socket.currentRoom) {
+          socket.leave(socket.currentRoom);
+        }
+
+        // socket.join the groupId room
+        socket.join(groupId);
+        socket.currentRoom = groupId;
+
+      } catch (error) {
+        console.error ('Error setting active group:', error);
+      }
+    });
 
     socket.on('shareLocation', (location) => {
       console.log(socket.decoded.username, 'location received.');
@@ -62,14 +94,19 @@ module.exports = function(io, state) {
         username: socket.decoded.username
       };
 
-      io.emit('updateLocations', sharedLocations);
+      // emit updateLocations to sockets in the room
+      io.to(socket.currentRoom).emit('updateLocations', sharedLocations);
       console.log('Locations object: ', sharedLocations);
     });
 
     socket.on('getLocations', () => {
-      io.emit('updateLocations', sharedLocations);
-    })
+      io.to(socket.currentRoom).emit('updateLocations', sharedLocations);
+    });
 
+    socket.on('disconnect_request', () => {
+      socket.disconnect();
+    });
+    
     socket.on('disconnect', async () => {
       console.log('User disconnected:', socket.decoded.username);
       delete sharedLocations[socket.decoded.id];
